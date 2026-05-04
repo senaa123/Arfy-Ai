267cf5bea98777f548719258bef56de45c7e58cb
Senan Jayasinghe <78232524+senaa123@users.noreply.github.com>
2026-05-04 14:24:07 +0530

    Delete memory_service directory

--- memory_service/summarizer.py
+++
@@ -1,7 +0,0 @@
-"""
-This file is optional for now.
-Later you can use Groq + LangChain here to summarize old conversation turns
-and store them in SummaryRecord.
-
-For v1, leave this as a placeholder or add summarization later.
-"""
